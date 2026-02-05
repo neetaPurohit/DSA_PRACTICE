45984fdcb0c371f386f9b41556ea816d7249e8e5
neeta purohit <neetapurohit56@gmail.com>
2026-02-05 19:34:45 +0530

    find shortest path in a string

---
+++ src/Strings/ShortestPath.java
@@ -0,0 +1,29 @@
+package Strings;
+
+public class ShortestPath {
+    public static float findShortestPath(String str){
+        int x =0, y = 0;
+        for (int i=0; i<str.length(); i++){
+            //stored each character to compare direction
+            char dir = str.charAt(i);
+//            sout
+            if (dir=='S'){
+                y--;
+            }else if (dir=='N'){  //north
+                y++;
+            }else if (dir=='W'){//west
+                x--;
+            }else {//east
+                x++;
+            }
+        }
+
+        int X2=x*x;
+        int Y2=y*y;
+        return (float)Math.sqrt(X2+Y2);
+    }
+    static void main(String[] args) {
+        String path = "WNEENESENNN";
+        System.out.println(findShortestPath(path));
+    }
+}
