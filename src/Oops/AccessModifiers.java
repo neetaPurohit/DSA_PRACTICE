7f6f59a4b836c783f8bfb0f04ee428afc16acd7a
neeta purohit <neetapurohit56@gmail.com>
2026-02-09 20:17:15 +0530

    Access modifiers

---
+++ src/Oops/AccessModifiers.java
@@ -0,0 +1,23 @@
+package Oops;
+
+public class AccessModifiers {
+
+    static void main(String[] args) {
+     BankAccount b1 = new BankAccount();
+     b1.username = "nikita";
+     b1.setPassword("#dfker"); //we only can set password using setter we can't access that password, to access we use getter
+        
+    }
+}
+
+class BankAccount{
+    public String username;
+    private String password;
+
+    void setUsername(String userName){
+        username = userName;
+    }
+    void setPassword(String Password){
+        password = Password;
+    }
+}
