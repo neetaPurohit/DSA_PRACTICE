5d8d126d68fbb296f8967e9d699c9d1a3b74ef81
neeta purohit <neetapurohit56@gmail.com>
2026-02-14 13:05:21 +0530

    Add inverted Star pattern

---
+++ src/Patterns/InvertedStarPattern.java
@@ -0,0 +1,13 @@
+package Patterns;
+
+public class InvertedStarPattern {
+    static void main(String[] args) {
+        int n=4;
+        for (int i=1; i<=4; i++){
+            for (int star=1; star<=n-i+1; star++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
