59df7d76ee51aff23c787e84038e434a0593a9b7
neeta purohit <neetapurohit56@gmail.com>
2026-02-19 10:55:28 +0530

    array is sorted or not

---
+++ src/Recursion/Sorted_Array.java
@@ -0,0 +1,17 @@
+package Recursion;
+
+public class Sorted_Array {
+    public static boolean isSorted(int[] arr, int i){
+//        base case
+        if (i == arr.length-1) return true;
+
+        if (arr[i] > arr[i+1]){
+            return false;
+        }
+        return isSorted(arr,i+1);
+    }
+    static void main(String[] args) {
+        int[] arr = {1,2,3,4};
+        System.out.println(isSorted(arr,0));
+    }
+}
