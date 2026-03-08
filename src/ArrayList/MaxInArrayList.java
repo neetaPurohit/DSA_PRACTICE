54cf4f9048d6f3a3c583d55732f70e1e0a3d0cf5
neeta purohit <neetapurohit56@gmail.com>
2026-03-08 21:04:37 +0530

    Find max using math function in ArrayList

--- src/ArrayList/MaxInArrayList.java
+++ src/ArrayList/MaxInArrayList.java
@@ -15,9 +15,11 @@ public class MaxInArrayList {
 
         int max = Integer.MIN_VALUE;
         for (int i=0; i<list.size(); i++){
-            if (max < list.get(i)){
-                max = list.get(i);
-            }
+//            if (max < list.get(i)){
+//                max = list.get(i);
+//            }
+
+            max = Math.max(max, list.get(i));
         }
         System.out.println("max element = " + max);
 
