90298df9a622bca52ab6bd5db4646426e836b385
neeta purohit <neetapurohit56@gmail.com>
2026-03-11 20:36:10 +0530

    Nested loop on lists

--- src/ArrayList/TableUsingArrayList.java
+++ src/ArrayList/TableUsingArrayList.java
@@ -23,5 +23,21 @@ public class TableUsingArrayList {
         mainList.add(list3);
 
         System.out.println(mainList);
+
+
+//        we can remove elements from the list
+        list2.remove(0);
+        list2.remove(1);
+
+//        printing all lists
+        for (int i=0; i<mainList.size(); i++){
+            ArrayList<Integer> currList = mainList.get(i);
+            for (int j=0; j<currList.size(); j++){
+                System.out.print(currList.get(j) + " ");
+            }
+            System.out.println();
+        }
+
+
     }
 }
