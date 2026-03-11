3584af9452315a050126a000c1cb81746a1dd474
neeta purohit <neetapurohit56@gmail.com>
2026-03-11 19:48:23 +0530

    Tables using multidimensional ArrayList

--- src/ArrayList/TableUsingArrayList.java
+++ src/ArrayList/TableUsingArrayList.java
@@ -2,7 +2,7 @@ package ArrayList;
 
 import java.util.ArrayList;
 
-public class MultiDimensionalArr {
+public class TableUsingArrayList {
     static void main(String[] args) {
 
         ArrayList<ArrayList<Integer>> mainList = new ArrayList<>();
@@ -12,7 +12,7 @@ public class MultiDimensionalArr {
         ArrayList<Integer> list2 = new ArrayList<>();
         ArrayList<Integer> list3 = new ArrayList<>();
 
-        for (int i=0; i<=5; i++){
+        for (int i=1; i<=5; i++){
             list1.add(i*1);//1 2 3 4 5
             list2.add(i*2);//2,4,6,8,10
             list3.add(i*3);//3,6,9,12,15
