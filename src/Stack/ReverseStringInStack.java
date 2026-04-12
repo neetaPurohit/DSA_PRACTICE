ced670f4a6cb5365f82cba642428bf1c960a2d45
neeta purohit <neetapurohit56@gmail.com>
2026-04-12 18:59:14 +0530

    Reverse a string using a stack

--- src/Stack/ReverseStringInStack.java
+++ src/Stack/ReverseStringInStack.java
@@ -1,4 +1,27 @@
 package Stack;
 
+import java.util.Stack;
+
 public class ReverseStringInStack {
+
+    public static String reverseStr(String str){
+        Stack<Character> s = new Stack<>();
+        int idx = 0;
+
+        while (idx < str.length()){
+            s.push(str.charAt(idx));
+            idx++;
+        }
+        StringBuilder result = new StringBuilder("");
+        while (!s.isEmpty()){
+            char curr = s.pop();
+            result.append(curr);
+        }
+        return result.toString();
+    }
+    static void main(String[] args) {
+        String str = "abc";
+        String result = reverseStr(str);
+        System.out.println(result);
+    }
 }
